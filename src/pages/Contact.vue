c1e23c19cfe5e3ffb220d5c129bf8bb7016efee7
LoViSTUDIO <lorenzovillafane@gmail.com>
2023-12-09 17:44:41 -0300

    avance en los estilos y en la estructura de contact

--- src/pages/Contact.vue
+++ src/pages/Contact.vue
@@ -1,3 +1,37 @@
-<template></template>
-<style scoped ></style>
+<template>
+    <header>
+      <router-link to="/">BACK TO MENU</router-link>
+    </header>
+    <main>
+    <div class="tabs">
+      <input class="input" name="tabs" type="radio" id="tab-1" checked="true" />
+      <label class="label" for="tab-1">CONTACT</label>
+      <div class="panel">
+        <h1></h1>
+    
+<form id="form" class="topBefore">
+		
+        <input id="name" type="text" placeholder="NAME">
+        <input id="email" type="email" placeholder="E-MAIL">
+        <textarea id="message" type="text" placeholder="MESSAGE"></textarea>
+<input id="submit" type="submit" value="GO!">
+
+</form>
+      </div>
+      <input class="input" name="tabs" type="radio" id="tab-2" />
+      <label class="label" for="tab-2">SOCIAL</label>
+      <div class="panel dos">
+        <a href=""><img src="../assets/icon/social/linkedin.png" alt="linkedin"></a>
+        <a href=""><img src="../assets/icon/social/telegram.png" alt="telegram"></a>
+        <a href=""><img src="../assets/icon/social/whatsapp.png" alt="wpp"></a>
+        <a href=""><img src="../assets/icon/social/github.png" alt="github"></a>
+        <a href=""><img src="../assets/icon/social/gmail.png" alt="gmail"></a>
+      </div>
+    </div>
+  </main>
+    
+    </template>
+<style scoped lang="scss">
+@import "../styles/contact.scss";
+</style>
 <script></script>
--- src/pages/Projects.vue
+++ src/pages/Projects.vue
@@ -5,19 +5,37 @@
   <main>
     <div class="tabs">
       <input class="input" name="tabs" type="radio" id="tab-1" checked="true" />
-      <label class="label" for="tab-1">ABOUT ME</label>
+      <label class="label" for="tab-1">PROFESSOR SLOWBRO</label>
       <div class="panel">
-        <h1>ABOUT ME</h1>
+        <h1>PROFESSOR SLOWBRO</h1>
+        <p>
+          Professor Slowbro es una web dedicada a Pokémon y su ecosistema
+          competitivo.
+        </p>
+        <div class="descripcionProfessor">
+          <img
+            src="../assets/projects/professorslowbro.png"
+            alt="professorSlowbro"
+          />
+        </div>
       </div>
-      <input class="input" name="tabs" type="radio" id="tab-2" />
-      <label class="label" for="tab-2">STUDIES</label>
-      <div class="panel dos"></div>
+
       <input class="input" name="tabs" type="radio" id="tab-3" />
-      <label class="label" for="tab-3">HOBBIES</label>
+      <label class="label" for="tab-3">TASKAPP</label>
       <div class="panel tres">
-        <h1>HOBBIES</h1>
+        <h1>TASKAPP</h1>
+        <p>
+          Taskapp es una aplicacion para registrar tus tareas y llevar un
+          registro de ellas.
+        </p>
+        <div class="descripcionTaskapp">
+          <img src="../assets/projects/taskapp.png" alt="taskapp" />
+        </div>
       </div>
     </div>
   </main>
 </template>
+<style scoped lang="scss">
+@import "../styles/projects.scss";
+</style>
 <script></script>
