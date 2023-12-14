8b63b9fbdcbd46fe16f8d0ae3bb0868b63f98879
LoViSTUDIO <lorenzovillafane@gmail.com>
2023-12-14 16:17:51 -0300

    navbar cambiada

--- src/pages/MenuDesktop.vue
+++ src/pages/MenuDesktop.vue
@@ -1,4 +1,15 @@
 <template>
+  <header>
+    <nav role="navigation" class="primary-navigation">
+  <ul>
+    <li><router-link to="/About">SOBRE MI</router-link></li>
+    <li><router-link to="/Skills">SKILLS</router-link></li>
+    <li><router-link to="/Projects">PROYECTOS</router-link></li>
+    <li><router-link to="/Contact">CONTACTO</router-link></li>
+    <li><p></p></li>
+  </ul>
+</nav>
+  </header>
   <main>
     <div class="container">
       <p>HOLA, SOY </p>
@@ -8,215 +19,95 @@
         <div class="third"><div>Game Developer</div></div>
       </section>
     </div>
-    <span><span></span></span>
-    <div class="wrap">
-      <router-link to="/Skills"
-        ><div>
-          <img
-            src="../assets/imagen/icons8-programación-96.png"
-            alt="skills"
-          />SKILLS
-        </div></router-link
-      >
-      <router-link to="/Projects"
-        ><div>
-          <img
-            src="../assets/imagen/icons8-documentos-de-producto-96.png"
-            alt="proyectos"
-          />PROYECTOS
-        </div></router-link
-      >
-      <router-link to="/Contact"
-        ><div>
-          CONTACTO<img
-            src="../assets/imagen/icons8-buzón-con-carta-96.png"
-            alt="contacto"
-          /></div
-      ></router-link>
-      <router-link to="/About"
-        ><div>
-          SOBRE MI<img
-            src="../assets/imagen/icons8-cuenta-de-prueba-96.png"
-            alt="sobremi"
-          /></div
-      ></router-link>
-    </div>
   </main>
 </template>
 <style scoped lang="scss">
-body,
-html {
-  margin: 0;
-  padding: 0;
-  height: 100%;
-  background-color: #243a73;
+/*NAVBAR*/
+nav {
+  &.primary-navigation {
+    margin: 0 auto;
+    display: block;
+  
+    padding: 20px;  
+    text-align: center;
+    font-size: 20px;
+    ul li p{
+      list-style: none;
+      margin: 0 auto;
+      border-left: 2px solid #B0A695;
+      display: inline-block;
+  
+      position: relative;
+      text-decoration: none;
+      text-align: center;
+      font-family: arvo;
+
+    }
+
+    ul li {
+      list-style: none;
+      margin: 0 auto;
+      border-left: 2px solid #B0A695;
+      display: inline-block;
+      padding: 0 30px;
+      position: relative;
+      text-decoration: none;
+      text-align: center;
+      font-family: arvo;
+    }
+
+    li a {
+      color: #3a332c;
+    }
+
+    li a:hover {
+      color: #776B5D;
+    }
+
+    li:hover {
+      cursor: pointer;
+    }
+
+    ul li:hover > ul,
+    ul li ul:hover {
+      visibility: visible;
+      opacity: 1;
+      display: block;
+      min-width: 250px;
+      text-align: left;
+      padding-top: 20px;
+      box-shadow: 0px 3px 5px -1px #ccc;
+    }
+
+    ul li ul li {
+      clear: both;
+      width: 100%;
+      text-align: left;
+      margin-bottom: 20px;
+      border-style: none;
+    }
+
+    ul li ul li a:hover {
+      padding-left: 10px;
+      border-left: 2px solid #3a332c;
+      transition: all 0.3s ease;
+    }
+  }
 }
-body {
-  background: #243a73;
-  box-shadow: inset 0 0 20vmin 0 #585247;
-}
-.wrap {
-  position: relative;
-  width: 40vmin;
-  height: 40vmin;
-  margin: 0 auto;
-  background: inherit;
-  transform: scale(0.2) translatez(0px);
-  opacity: 0;
-  transition: transform 0.5s, opacity 0.5s;
-}
+
 a {
-  position: absolute;
-  left: 0;
-  top: 0;
-  width: 47.5%;
-  height: 47.5%;
-  overflow: hidden;
-  transform: scale(0.5) translateZ(0px);
-  background: #f2ebe9;
-}
-a div {
-  height: 100%;
-  background-size: cover;
-  opacity: 0.5;
-  transition: opacity 0.5s;
-  border-radius: inherit;
-}
-a:nth-child(1) {
-  border-radius: 40vmin 0 0 0;
-  transform-origin: 110% 110%;
-  transition: transform 0.4s 0.15s;
-  text-decoration: none;
-  font-weight: bold;
-}
-a:nth-child(1) div {
-  background-color: #BBAB8C;
-  background-size: 360px;
-  font-size: 25px;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  align-content: center;
-  flex-direction: column;
-  text-decoration: none;
-  font-size: bold;
-  color: #3a332c;
-}
-a:nth-child(2) {
-  border-radius: 0 40vmin 0 0;
-  left: 52.5%;
-  font-weight: bold;
-  transform-origin: -10% 110%;
-  transition: transform 0.4s 0.2s;
-  text-decoration: none;
-}
-a:nth-child(2) div {
-  background-color: #BBAB8C;
-  background-size: 360px;
-  font-size: 25px;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  align-content: center;
-  flex-direction: column;
-  text-decoration: none;
-  font-size: bold;
-  color: #3a332c;
-}
-a:nth-child(3) {
-  border-radius: 0 0 0 40vmin;
-  top: 52.5%;
-  transform-origin: 110% -10%;
-  font-weight: bold;
-  text-decoration: none;
-  transition: transform 0.4s 0.25s;
-}
-a:nth-child(3) div {
-  background-color: #BBAB8C;
-  background-size: 360px;
-  font-size: 25px;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  align-content: center;
-  flex-direction: column;
-  text-decoration: none;
-  font-size: bold;
-  color: #3a332c;
-}
-a:nth-child(4) {
-  border-radius: 0 0 40vmin 0;
-  top: 52.5%;
-  left: 52.5%;
-  text-decoration: none;
-  font-weight: bold;
-  transform-origin: -10% -10%;
-  transition: transform 0.4s 0.3s;
-}
-a:nth-child(4) div {
-  background-color: #BBAB8C;
-  background-size: 360px;
-  font-size: 25px;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  align-content: center;
-  flex-direction: column;
-  text-decoration: none;
-  font-size: bold;
-  color:#3a332c;
-}
-a:nth-child(4) div img {
-  width: 84px;
+
+    text-decoration: none;
+
+    &:hover {
+        color: #3CA0E7;
+    }
+ 
 }
 
-span {
-  position: relative;
-  display: block;
-  margin: 0 auto;
-  top: 25vmin;
-  width: 10vmin;
-  height: 10vmin;
-  border-radius: 100%;
-  background: #776b5d;
-  transform: translateZ(0px);
-}
-span span {
-  position: absolute;
-  width: 60%;
-  height: 3px;
-  background: #f2ebe9;
+ ul li ul li a { transition: all 0.5s ease; }
 
-  left: 20%;
-  top: 50%;
-  border-radius: 0;
-}
-span span:after,
-span span:before {
-  content: "";
-  position: absolute;
-  left: 0;
-  top: -1.5vmin;
-  width: 100%;
-  height: 100%;
-  background: inherit;
-}
-span span:after {
-  top: 1.5vmin;
-}
-span:hover + .wrap,
-.wrap:hover {
-  transform: scale(0.8) translateZ(0px);
-  opacity: 1;
-}
-span:hover + .wrap a,
-.wrap:hover a {
-  transform: scale(1) translatez(0px);
-}
-a:hover div {
-  opacity: 1;
-  transform: translatez(0px);
-}
+
 
 /*ESTILO H1 */
 
